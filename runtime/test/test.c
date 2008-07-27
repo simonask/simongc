313ee0c9a53ba08e764f029418b333fc19684126
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-07-27 17:32:44 +0200

    updated test

--- runtime/test/test.c
+++ runtime/test/test.c
@@ -6,7 +6,8 @@ int main (int argc, char const *argv[])
 	simon_gc_initialize(0);
 	
 	char* lol = (char*)simon_gc_malloc(1024);
-	StackFrame* frame = simon_gc_roots(1, &lol);
+	printf("allocated 'lol' at 0x%x\n", lol);
+	simon_gc_stack_push(1, &lol);
 	
 	size_t i;
 	for (i = 0; i < 1024; ++i)
@@ -15,7 +16,23 @@ int main (int argc, char const *argv[])
 	}
 	printf("Yay: %s\n", lol);
 	
-	simon_gc_cleanup_roots(frame);
+	ObjectHeader* header = NULL;
+	void* data;
+	while (simon_gc_walk_objects(&header, &data))
+	{
+		printf("Object at 0x%x (size: %d)\n", data, header->size);
+	}
+	
+	simon_gc_collect();
+	simon_gc_stack_pop();
+	simon_gc_collect();
+	header = NULL;
+	while (simon_gc_walk_objects(&header, &data))
+	{
+		// Should be none.
+		if (header->flags & OBJECT_UNREACHABLE)
+			printf("Object at 0x%x (size: %d) is unreachable!\n", data, header->size);
+	}
 	
 	return 0;
 }
