c1c0ebfdbe8194f04a7399abe23965750680f38a
Simon Ask Ulsnes <simon@ulsnes.dk>
2008-07-27 22:18:13 +0200

    Updated test.

--- runtime/test/test.c
+++ runtime/test/test.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include "SimonGCRuntime.h"
 
+#include <assert.h>
+
+void finalizer(void* data)
+{
+	printf("Finalizer called for 0x%x!\n", data);
+}
+
 int main (int argc, char const *argv[])
 {
-	simon_gc_initialize(0);
+	simon_gc_initialize(1024 * 8);
 	
 	char* lol = (char*)simon_gc_malloc(1024);
-	printf("allocated 'lol' at 0x%x\n", lol);
+	assert(lol && "Out of memory, or error in simon_gc_malloc :(");
 	simon_gc_stack_push(1, &lol);
 	
 	size_t i;
@@ -14,13 +21,20 @@ int main (int argc, char const *argv[])
 	{
 		lol[i] = 'a' + (i % ('z' - 'a'));
 	}
-	printf("Yay: %s\n", lol);
+	
+	char* buffer = (char*)simon_gc_malloc(1024);
+	while (buffer)
+	{
+		simon_gc_stack_push(1, &buffer);
+		buffer = (char*)simon_gc_malloc(1024);
+		simon_gc_stack_pop();
+	}
 	
 	ObjectHeader* header = NULL;
 	void* data;
 	while (simon_gc_walk_objects(&header, &data))
 	{
-		printf("Object at 0x%x (size: %d)\n", data, header->size);
+		printf("Object at 0x%x (size: %d, header: 0x%x)\n", data, header->size, header);
 	}
 	
 	simon_gc_collect();
